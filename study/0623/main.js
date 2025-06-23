ca81c959acc49fd465800ed63f840510fbd2a6c7
dhyunii96 <christopher9645@gmail.com>
2025-06-23 17:39:55 +0900

    0623

---
+++ study/0623/main.js
@@ -0,0 +1,127 @@
+// alert("Hello World");
+// console.log("자바스크립트는 객체 기반입니다!");
+// console.log("데이터 타입 선언 없이 변수 생성 가능!");
+// document.write("<h1>Documet Write</h1>");
+
+// let currentYear = 2025;
+// let birthYear = prompt;
+// let age = currentYear - birthYear + 1;
+// alert("당신의 나이는" + age + "살 입니다.");
+
+// let name = "철수";
+// let age = 18;
+// let isStudent = true;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent)
+
+// let price = 10000;
+// let rate = 0.2;
+// let discount = price * rate;
+// let finalPrice = price - discount;
+
+// console.log("할인된 가격은" + finalPrice + "원입니다.")
+
+// let name = "영희";
+// console.log(`안녕하세요, ${name}님!`);
+// console.log("안녕하세요, " + name + "님!")
+
+// let a =10;
+// console.log(a);
+// a += 5;
+// console.log(a);
+// a -=3;
+// console.log(a);
+// a *=2;
+// console.log(a);
+// a /=4;
+// console.log(a);
+// console.log(`최종 값은 ${a}입니다`)
+
+
+
+// let num = prompt("숫자를 입력하세요");
+// if (num % 3 == 0){
+//     alert("3의 배수입니다.");
+// } else {
+//     alert("3의 배수가 아닙니다.");
+// }
+
+
+// let day = prompt("요일을 입력하세요");
+// switch (day) {
+//     case "월요일":
+//         alert("한 주의 시작!");
+//         break;
+//     case "금요일":
+//         alert("불타는 금요일!");
+//         break;
+//     default:
+//         alert("평번한 하루네요.")
+// }
+
+
+
+// let name = "홍길동";
+// let age = "20";
+
+// alert(name + "님은 " + age + "살입니다.");
+
+// 연습문제
+
+// 1. 숫자 두 개를 변수로 저장하고 더한 결과를 출력해보세요.
+
+// let a = 5;
+// let b = 3;
+
+// c = b + a;
+// alert(c);
+
+
+// 2. 이름과 나이를 저장하고 다음과 같은 문장을 출력해보세요. "홍길동님은 20살입니다."
+
+// let name = "홍길동";
+// let age = 20;
+
+// alert(`${name}님은 ${age}살입니다.`);
+
+
+// // 3. 정사각형의 한 변 길이를 변수로 저장하고 넓이를 출력해보세요.
+
+// let side = 4;
+
+// alert(4 * 4);
+
+
+// 4. 숫자 하나가 짝수인지 홀수인지 출력해보세요.
+
+// let number = 7;
+
+// if (number % 2 === 0) {
+//   alert("짝수입니다.");
+// } else {
+//   alert("홀수입니다.");
+// }
+
+
+// 5. 나이에 따라 아래 문장을 출력해보세요.
+
+// 19세 미만: "미성년자입니다."
+// 19세 이상: "성인입니다."
+// let age = 16;
+
+// if (age < 19) {
+//   alert("미성년자입니다.");
+// } else {
+//   alert("성인입니다.");
+// }
+
+
+// 6. 비밀번호가 맞는지 확인하는 코드를 작성해보세요.
+
+// let password = "1234";
+// let input = "1234"; 
+
+// if (password === input) alert("로그인 성공");
+// else alert("로그인 실패");
