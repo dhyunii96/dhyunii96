1688cdb44250c366b5bf632e4770ec7e8b1e2d4b
dhyunii96 <christopher9645@gmail.com>
2025-07-08 20:01:06 +0900

    0708

---
+++ study/0708/ch01_5/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as D from "./data";
+import { Component } from "react";
+import ClassComponent from './ClassComponent';
+
+function App() {
+  console.log("App called.");
+  return <h1>Hello World!!!</h1>;
+}
+
+export default App;
---
+++ study/0708/ch01_5/src/ClassComponent.tsx
@@ -0,0 +1,17 @@
+import { Component, ReactNode } from "react";
+
+
+
+export default class ClassComponent extends Component {
+    render() {
+        return(
+            <li>
+                <a href="https://www.google.com">
+                    <p>go to Google</p>
+                </a>
+            </li>
+        )
+    }
+}
+
+
